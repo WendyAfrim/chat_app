f2216bc38809d40d91c03e05aa2979c1fcc57e51
Wendy Afrim <wendy.afrim2@gmail.com>
2022-12-13 09:56:56 +0100

    Integrate login view

--- sources/client/router.js
+++ sources/client/router.js
@@ -14,6 +14,7 @@ app.use('/', express.static(__dirname + '/home' ));
 
 app.use('/chat', express.static(__dirname + '/chat' ));
 
+app.use('/login', express.static(__dirname + '/login' ));
 
 // app.get('/', (req, res) => {
 //     res.sendFile(__dirname +'/index.html');
